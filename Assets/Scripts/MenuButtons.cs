79c7bb2c9205661dfbbc38e1e0667a8dd3e77bd4
A1exdV <alexdvapple@gmail.com>
2022-05-13 18:43:41 +0300

    menu bunnots script created

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -1,18 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject aboutGame;
+    [SerializeField] private GameObject mainMenu;
+
+    [SerializeField] private GameObject slide1;
+    [SerializeField] private GameObject slide2;
+
+    [SerializeField] private TextMeshProUGUI prevButton;
+    [SerializeField] private TextMeshProUGUI nextButton;
+
+    public void  PlayButton()
     {
-        
+        mainMenu.SetActive(false);
+        aboutGame.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NextButton()
     {
-        
+        if (slide1.activeInHierarchy)
+        {
+            slide1.SetActive(false);
+            slide2.SetActive(true);
+            prevButton.text = "Prev";
+            nextButton.text = "Play";
+        }
+        else
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+
+    public void PrevButton()
+    {
+        if (slide2.activeInHierarchy)
+        {
+            slide2.SetActive(false);
+            slide1.SetActive(true);
+            prevButton.text = "Return";
+            nextButton.text = "Next";
+        }
+        else
+        {
+            aboutGame.SetActive(false);
+            mainMenu.SetActive(true);
+
+        }
     }
 }
